f93d1fdb6be67c87f2902455b8886e004db21fd4
Nico Aldana <41879019+reyluno@users.noreply.github.com>
2023-02-10 15:41:06 -0500

    progress 02.09

--- venv/laser-cut-container/master.py
+++ venv/laser-cut-container/master.py
@@ -3,7 +3,7 @@
 # User inputted variables
 d = float(input("Please input an overall depth for the container: ")) #Recommended: 100
 L = float(input("Please input an overall length for the container: ")) #Recommended: 125-150
-h = float(input("Please input a height for the pencil holder: ")) # Recommended: 220 - r
+hp = float(input("Please input a height for the pencil holder: ")) # Recommended: 220 - r
 
 # Global variables
 H = 300 # Overall height
@@ -11,7 +11,8 @@ t = 3.175 # Acrylic thickness
 
 # Derived variables
 r = d/2 # Half overall width
-h = H - r
+l = L - r # Length of pencil-holder segment
+h = H - r # Vertical height of spine
 dp = d-2*t # Adjusted piece width
 
 # Header for svg document
@@ -22,9 +23,6 @@ output = starter
 # Create base piece svg
 def Base(): 
     baseOutput = output # Instantiate svg with the same basic starter
-    # Derived measurements:
-    l = L - r # Length of pencil-holder segment
-
     baseOutput += f'<path d="M5 5 l{dp} 0 l0 {l} l{t} 0 a{r} {r} 0 0 1 {-2*r} 0 l{t} 0 l{0} {-l}" stroke="black" stroke-width="1" fill="none"/>'
     # Cut out tab for spine
     baseOutput += f'<rect x="{5+dp/3}" y="{5+l-t}" width="{dp/3}" height="{t}" stroke="black" stroke-width="1" fill="none"/>'
@@ -36,7 +34,6 @@ def Base():
 # Create spine piece svg
 def Spine():
     # Derived measurements:
-    h = H - r
     rectX = 5 + (d-20)/2
     rectY = 5 + r/3
     spineOutput = output
@@ -44,10 +41,27 @@ def Spine():
     spineOutput += f'<rect x="{rectX}" y="{rectY}" width="20" height="{t}" stroke="black" stroke-width="1" fill="none"/>'
     # ADD CIRCLES FOR SCREW HOLES HERE
 
+    # ADD HOOK PIECE CUTOUT
+    #spineOutput += f'<path d="" stroke="black" stroke-width="1" fill="none"/>'
+
     spineOutput += '\n</svg>'
     f = open('spine.svg', 'w')
     f.write(spineOutput)
 
+def Sides():
+    sideOutput = output
+    sideOutput += f'<rect x="5" y="5" width="{l}" height="{h}" stroke="black" stroke-width="1" fill="none" />'
+    sideOutput += f'<rect x="{5 + l}" y="5" width="{l}" height="{h}" stroke="black" stroke-width="1" fill="none" />'
+    sideOutput += f'<rect x="{5 + 2*l}" y="5" width="{d}" height="{h}" stroke="black" stroke-width="1" fill="none" />' # Front panel
+
+    # ADD CIRCLES AND SCREW CONTOURS
+
+    sideOutput += '\n</svg>'
+    f = open('sides.svg', 'w')
+    f.write(sideOutput)
+
+
 # Create 
 Base()
 Spine()
+Sides()
